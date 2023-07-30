85ce5b6533ad3f9f91c8bf04d839fe48a3b7a8f0
ReDark Technology <namarahasia12345@gmail.com>
2023-07-30 13:38:39 +0700

    Updated project settings

--- ItakuDesktop/Properties/AssemblyInfo.cs
+++ ItakuDesktop/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ItakuDesktop")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A silly little app I made")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BunzStudio")]
 [assembly: AssemblyProduct("ItakuDesktop")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
